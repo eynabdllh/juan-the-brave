e69b77264d04a8b938bcc2c5ecae252842257598
eynabdllh <abdullanamerah@gmail.com>
2025-09-12 16:28:00 +0800

    Add player attack system and enemy damage handling

--- scripts/enemylevel_1.gd
+++ scripts/enemylevel_1.gd
@@ -8,7 +8,12 @@ var player = null
 # This variable will remember the last direction the enemy was moving
 var last_animation = "front_walk"
 
+var health = 100
+var player_inattack_zone = false
+var can_take_damage = true
+
 func _physics_process(delta):
+	deal_with_damage()
 	if player_chase and player != null:
 		# --- CHASE LOGIC ---
 		var direction = (player.position - position).normalized()
@@ -49,3 +54,27 @@ func _on_detection_area_body_exited(body: Node2D) -> void:
 		$AnimatedSprite2D.play(last_animation) # Select the last animation
 		$AnimatedSprite2D.set_frame(0)         # Go to its first frame
 		$AnimatedSprite2D.stop()               # and stop it.
+
+func enemylevel_1():
+	pass
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+
+func deal_with_damage():
+	if player_inattack_zone and global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("enemy health: ", health)
+			if health <= 0:
+				self.queue_free()
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,8 @@ var enemy_attack_cooldown = true
 var health = 100
 var player_alive = true
 
+var attack_ip = false
+
 const speed = 100
 var current_dir = "none"
 
@@ -15,6 +17,7 @@ func _physics_process(delta):
 	player_movement(delta)
 	multiDirCheck()
 	enemy_attack()
+	attack()
 	
 	if health <= 0:
 		player_alive = false #go back to menu or respond
@@ -73,28 +76,32 @@ func play_anim(movement):
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
-			anim.play("side_idle")
+			if attack_ip == false:
+				anim.play("side_idle")
 
 	if dir == "left":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
-			anim.play("side_idle")
+			if attack_ip == false:
+				anim.play("side_idle")
 
 	if dir == "down":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("front_walk")
 		elif movement == 0:
-			anim.play("front_idle")
+			if attack_ip == false:
+				anim.play("front_idle")
 
 	if dir == "up":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("back_walk")
 		elif movement == 0:
-			anim.play("back_idle")
+			if attack_ip == false:
+				anim.play("back_idle")
 
 func player():
 	pass
@@ -103,22 +110,47 @@ func _on_player_hitbox_body_entered(body: Node2D) -> void:
 	# This first print statement will tell us if the collision is detected AT ALL.
 	print("Something entered my hitbox! It was: ", body.name)
 
-	if body.is_in_group("enemies"):
+	if body.has_method("enemylevel_1"): 
 		# This second print statement will tell us if the group check passed.
 		print("The thing that entered was an enemy!")
-		enemy_inattack_range = true
+		enemy_inattack_range = true	
 
 func _on_player_hitbox_body_exited(body: Node2D) -> void:
-	if body.is_in_group("enemies"): 
+	if body.has_method("enemylevel_1"): 
 		enemy_inattack_range = false
 
 func enemy_attack():
 	if enemy_inattack_range and enemy_attack_cooldown == true:
-		health -= 20
+		health = health - 1
 		enemy_attack_cooldown = false
 		$attack_cooldown.start()
 		print(health)
 
-
 func _on_attack_cooldown_timeout() -> void:
 	enemy_attack_cooldown = true
+
+func attack():
+	var dir = current_dir
+	
+	if Input.is_action_just_pressed("attack"):
+		global.player_current_attack = true
+		attack_ip = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "left":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "down":
+			$AnimatedSprite2D.play("front_attack")
+			$deal_attack_timer.start()
+		if dir == "up":
+			$AnimatedSprite2D.play("back_attack")
+			$deal_attack_timer.start()
+
+func _on_deal_attack_timer_timeout():
+	$deal_attack_timer.stop()
+	global.player_current_attack = false
+	attack_ip = false
